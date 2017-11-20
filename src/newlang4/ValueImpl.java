d99e066cbf47363a795fc870342fa7aace667243
Nekonoha <tan.fantazma@gmail.com>
2017-11-20 21:27:33 +0900

    value

--- src/newlang4/ValueImpl.java
+++ src/newlang4/ValueImpl.java
@@ -6,89 +6,46 @@ import newlang3.ValueType;
 public class ValueImpl implements Value {
     ValueType valueType;
     String s;
-    int i;
-    double d;
-    boolean b;
 
 
-    public ValueImpl(String s){
+    public ValueImpl(String s) {
         this.s = s;
         this.valueType = ValueType.STRING;
     }
 
-    public ValueImpl(int i){
-        this.i = i;
+    public ValueImpl(int i) {
+        this.s = String.valueOf(i);
         this.valueType = ValueType.INTEGER;
     }
 
-    public ValueImpl(double d){
-        this.d = d;
+    public ValueImpl(double d) {
+        this.s = String.valueOf(d);
         this.valueType = ValueType.DOUBLE;
     }
 
-    public ValueImpl(boolean b){
-        this.b = b;
+    public ValueImpl(boolean b) {
+        this.s = String.valueOf(b);
         this.valueType = ValueType.BOOL;
     }
 
     @Override
     public String getSValue() {
-        switch (valueType) {
-            case STRING:
-                return s;
-            case INTEGER:
-                return String.valueOf(i);
-            case DOUBLE:
-                return String.valueOf(d);
-            case BOOL:
-                return String.valueOf(b);
-        }
-        return null;
+        return s;
     }
 
     @Override
     public int getIValue() {
-        switch (valueType) {
-            case STRING:
-                return Integer.parseInt(s);
-            case INTEGER:
-                return i;
-            case DOUBLE:
-                return (int)d;
-            case BOOL:
-            break;
-        }
-        return -1;
+        return Integer.parseInt(s);
     }
 
     @Override
     public double getDValue() {
-        switch (valueType) {
-            case STRING:
-                return Double.parseDouble(s);
-            case INTEGER:
-                return (double)i;
-            case DOUBLE:
-                return (int)d;
-            case BOOL:
-                break;
-        }
-        return -1;
+        return Double.parseDouble(s);
     }
 
     @Override
     public boolean getBValue() {
-        switch (valueType) {
-            case STRING:
-                return Boolean.parseBoolean(s);
-            case INTEGER:
-                break;
-            case DOUBLE:
-                break;
-            case BOOL:
-                return b;
-        }
-        return false;
+        return Boolean.parseBoolean(s);
     }
 
     @Override
